4c2010d5c245c25fbb6cac12a5508e1092cbee17
Markus <markusaksli@hotmail.com>
2019-11-03 19:44:53 +0200

    Added jumping and gliding in gameplay scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,30 @@
 
 public class PlayerController : MonoBehaviour
 {
-    private float InputX, InputZ, Speed;
+    private float InputX, InputZ, Speed, jumpVelocity;
     private CharacterController CC;
     private Camera cam;
     private Animator anim;
     private Vector3 desLookDir;
     private Vector3 desMoveDir;
-    private float gravity;
+
+    public float gravity;
+
+    public enum PlayerState { Default, Aiming, Rolling, Flying, Falling, Jumping };
+    public PlayerState currentState;
 
     [SerializeField] float rotationSpeed = 0.3f;
     [SerializeField] float moveSpeed = 1f;
     [SerializeField] float allowRotation = 0.1f;
     [SerializeField] float maxGravity = 100f;
+    [SerializeField] float flyGravity = 100f;
+    [SerializeField] float maxJumpVelocity = 100f;
+    [SerializeField] float jumpVelocityAcc = 100f;
     [SerializeField] float accGravity = 9.8f;
 
     void Start()
     {
+        currentState = PlayerState.Default;
         CC = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
         cam = Camera.main;
@@ -25,26 +33,105 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Gravity();
-
-        if (Input.GetMouseButton(1))
-        {
-            anim.SetBool("Aiming", true);
-        }
-        else
-        {
-            anim.SetBool("Aiming", false);
-        }
         InputX = Input.GetAxis("Horizontal");
         InputZ = Input.GetAxis("Vertical");
         InputDecider();
     }
 
+
+    void InputDecider()
+    {
+        //Start aiming if in default
+        if (currentState == PlayerState.Default)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                anim.SetBool("Aiming", true);
+                currentState = PlayerState.Aiming;
+            }
+        }
+        //Jump and Fly
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Only jump if in default
+            if (currentState == PlayerState.Default)
+            {
+                currentState = PlayerState.Jumping;
+            }
+            //Start falling if flying
+            else if (currentState == PlayerState.Flying)
+            {
+                currentState = PlayerState.Falling;
+            }
+            //Start flying if falling
+            else if (currentState == PlayerState.Falling)
+            {
+                currentState = PlayerState.Flying;
+            }
+        }
+
+
+        switch (currentState)
+        {
+            case PlayerState.Default:
+                Gravity();
+                anim.SetBool("Aiming", false);
+                anim.SetBool("Flying", false);
+                jumpVelocity = 0;
+                DefaultMovement();
+                break;
+
+            case PlayerState.Aiming:
+                Gravity();
+                anim.SetBool("Aiming", true);
+                AimMovement();
+                break;
+
+            case PlayerState.Rolling:
+                break;
+
+            case PlayerState.Flying:
+                anim.SetBool("Flying", true);
+                DefaultMovement();
+                Fly();
+                break;
+
+            case PlayerState.Jumping:
+                anim.SetBool("Jump", true);
+                Jump();
+                DefaultMovement();
+                break;
+
+            case PlayerState.Falling:
+                Gravity();
+                anim.SetBool("Jump", false);
+                anim.SetBool("Grounded", false);
+                anim.SetBool("Flying", false);
+                DefaultMovement();
+                break;
+        }
+
+    }
+
     void Gravity()
     {
+        if (gravity < -maxGravity)
+        {
+            anim.SetBool("Grounded", false);
+            currentState = PlayerState.Falling;
+        }
+        else
+        {
+            anim.SetBool("Grounded", true);
+        }
+
         if (CC.isGrounded)
         {
-            gravity = -0.5f * Time.deltaTime;
+            gravity = -0.1f;
+            if (currentState == PlayerState.Falling)
+            {
+                currentState = PlayerState.Default;
+            }
         }
         else
         {
@@ -54,24 +141,48 @@ public class PlayerController : MonoBehaviour
             }
         }
         CC.Move(new Vector3(0, gravity, 0) * Time.deltaTime);
-        if (gravity < -3f)
-        {
-            anim.SetBool("Grounded", false);
-        }
-        else
-        {
-            anim.SetBool("Grounded", true);
-        }
     }
 
-    void InputDecider()
+    void DefaultMovement()
     {
-        Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+        Speed = new Vector2(InputX, InputZ).normalized.sqrMagnitude;
         if (Speed > allowRotation)
         {
-            PlayerRotation();
-            PlayerMovement();
-            anim.SetFloat("InputMagnitude", Speed);
+
+            var forward = cam.transform.forward;
+            var right = cam.transform.right;
+            forward.y = 0;
+            right.y = 0;
+            right.Normalize();
+            forward.Normalize();
+
+            desLookDir = forward * InputZ + right * InputX;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desLookDir), rotationSpeed);
+
+            if (currentState == PlayerState.Default)
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    desMoveDir = transform.forward.normalized * (Time.deltaTime * moveSpeed * 1.5f);
+                    anim.SetFloat("InputMagnitude", Speed);
+                }
+                else
+                {
+                    desMoveDir = transform.forward.normalized * (Time.deltaTime * moveSpeed);
+                    Speed = Speed / 1.5f;
+                    anim.SetFloat("InputMagnitude", Speed);
+                }
+            }
+            else if (currentState == PlayerState.Flying)
+            {
+                desMoveDir = transform.forward.normalized * (Time.deltaTime * moveSpeed * 2);
+            }
+            else
+            {
+                desMoveDir = transform.forward.normalized * (Time.deltaTime * moveSpeed);
+            }
+            desMoveDir.y = 0;
+            CC.Move(desMoveDir);
         }
         else
         {
@@ -80,23 +191,68 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void PlayerRotation()
+    void AimMovement()
     {
-        var forward = cam.transform.forward;
-        var right = cam.transform.right;
-        forward.y = 0;
-        right.y = 0;
-        right.Normalize();
-        forward.Normalize();
+        if (!Input.GetMouseButton(1))
+        {
+            anim.SetBool("Aiming", false);
+            currentState = PlayerState.Default;
+        }
 
-        desLookDir = forward * InputZ + right * InputX;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desLookDir), rotationSpeed);
+        float lookX = Input.GetAxis("Mouse X");
+        transform.Rotate(Vector3.up, lookX * Time.deltaTime * rotationSpeed * 1000);
+
+        Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+        if (Speed > allowRotation)
+        {
+            var forward = cam.transform.forward;
+            var right = cam.transform.right;
+            forward.y = 0;
+            right.y = 0;
+            right.Normalize();
+            forward.Normalize();
+            desMoveDir = forward * InputZ + right * InputX;
+            desMoveDir = desMoveDir.normalized * Time.deltaTime * moveSpeed;
+            CC.Move(desMoveDir);
+        }
+        else
+        {
+            desMoveDir = Vector3.zero;
+        }
     }
 
-    void PlayerMovement()
+    void Jump()
     {
-        desMoveDir = transform.forward.normalized * (Time.deltaTime * moveSpeed);
-        desMoveDir.y = 0;
-        CC.Move(desMoveDir);
+        anim.SetBool("Grounded", false);
+        if (jumpVelocity < maxJumpVelocity)
+        {
+            jumpVelocity = Mathf.Lerp(jumpVelocity, maxJumpVelocity, jumpVelocityAcc * Time.deltaTime);
+        }
+        else
+        {
+            currentState = PlayerState.Falling;
+        }
+        CC.Move(new Vector3(0, jumpVelocity, 0));
     }
+
+    void Fly()
+    {
+        if (gravity > -flyGravity)
+        {
+            gravity -= accGravity * Time.deltaTime;
+        }
+        else if (gravity < -flyGravity)
+        {
+            gravity += accGravity * Time.deltaTime;
+        }
+
+        CC.Move(new Vector3(0, gravity, 0) * Time.deltaTime);
+
+        if (CC.isGrounded)
+        {
+            currentState = PlayerState.Default;
+            anim.SetBool("Grounded", true);
+        }
+    }
+
 }
