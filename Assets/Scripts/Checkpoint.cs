34ed713e0b5b0cd0c815e2ce3aae62530909141a
Markus <markusaksli@hotmail.com>
2019-11-19 22:11:37 +0200

    Reset ver change, reverted audio folder changes

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public PlayerController playerDeathManager;
+
+    public Renderer theRend;
+
+    public Material cpOff;
+    public Material cpOn;
+
+
+    private void Start()
+    {
+        playerDeathManager = FindObjectOfType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        
+    }
+    public void CheckpointOn()
+    {
+        Checkpoint[] checkpoints = FindObjectsOfType<Checkpoint>();
+        foreach (Checkpoint cp in checkpoints)
+        {
+            cp.CheckpointOff();
+        }
+
+        theRend.material = cpOn;
+    }
+    public void CheckpointOff()
+    {
+        theRend.material = cpOff;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            playerDeathManager.SetSpawnPoint(transform.position);
+            CheckpointOn();
+        }
+    }
+
+}
